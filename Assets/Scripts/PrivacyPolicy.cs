195f8550037409c6569195a56653df88acb9f5c6
Jakub Szaredko <jakub.szaredko@gmail.com>
2020-11-07 17:00:15 +0100

    Add privacy policy window

---
+++ Assets/Scripts/PrivacyPolicy.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+[RequireComponent(typeof(CanvasGroup))]
+public class PrivacyPolicy : MonoBehaviour
+{
+    public string privacyPolicyLink;
+    public string privacyPolicyAcceptedKey = "privacy_policy_accepted";
+
+    CanvasGroup cg;
+    
+    void Awake()
+    {
+        cg = GetComponent<CanvasGroup>();
+
+        if (PlayerPrefs.HasKey(privacyPolicyAcceptedKey))
+        {
+            cg.alpha = 0;
+            cg.interactable = cg.blocksRaycasts = false;
+        }
+        else
+        {
+            cg.alpha = 1;
+            cg.interactable = cg.blocksRaycasts = true;
+        }
+
+        StartCoroutine(WaitForRebuild());
+    }
+
+    public void OpenPrivacyPolicy()
+    {
+        Application.OpenURL(privacyPolicyLink);
+    }
+
+    public void OnAccept()
+    {
+        PlayerPrefs.SetInt(privacyPolicyAcceptedKey, 1);     
+
+        LeanTween.value(gameObject, v => { cg.alpha = v; }, 1, 0, 0.33f)
+            .setEaseInOutCubic()
+            .setIgnoreTimeScale(true)
+            .setOnComplete(() => {
+                cg.interactable = cg.blocksRaycasts = false;
+                Game.Instance.SignInGPGS();
+            });
+    }
+
+    IEnumerator WaitForRebuild()
+    {
+        yield return new WaitForSecondsRealtime(0.1f);
+
+        LayoutRebuilder.MarkLayoutForRebuild(transform.GetChild(0).GetComponent<RectTransform>());
+    }
+}
